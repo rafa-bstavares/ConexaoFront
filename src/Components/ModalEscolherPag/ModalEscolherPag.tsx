e9a95b2bcdbcdbb2d214fafb0d940e149ce574e4
rafaTavDev <rafabstavares@gmail.com>
2024-07-30 10:30:52 -0300

    seta voltar modal escolher

--- src/Components/ModalEscolherPag/ModalEscolherPag.tsx
+++ src/Components/ModalEscolherPag/ModalEscolherPag.tsx
@@ -2,6 +2,7 @@
 import { useContext } from "react"
 import Botao from "../Botao/Botao"
 import { ContextoPagamento } from "../../Contexts/ContextoPagamento/ContextoPagamento"
+import seta from "../../assets/images/setaSeletor.svg"
 
 export default function ModalEscolherPag(){
 
@@ -10,6 +11,10 @@ export default function ModalEscolherPag(){
     return(
         <div className="fixed bg-white/90 h-screen w-full top-0 left-0 flex justify-center items-center">
             <div className="flex flex-col items-center justify-center gap-4 lg:gap-8 px-4 lg:px-8 py-3 lg:py-6 bg-roxoPrincipal rounded-xl w-[90%] lg:w-1/2 text-white">
+                <div className="w-full flex gap-2 p-2" onClick={() => setAbrirModalEscolher(false)}>
+                    <img src={seta} alt="seta voltar" className="h-12 w-auto" />
+                    <div>Voltar</div>
+                </div>
                 <div className="text-white text-xl lg:text-4xl text-center">
                     Escolha a forma de pagamento
                 </div>
