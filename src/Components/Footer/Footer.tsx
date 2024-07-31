1d6a45e21ca39db486c68ecdb80b93e30751db67
rafaTavDev <rafabstavares@gmail.com>
2024-07-31 11:59:41 -0300

    link no rodape 04

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer(){
     return (
         <footer className="bg-roxoSecundario px-[var(--paddingXGeralCel)] lg:px-[var(--paddingXGeral)] lg:py-[var(--paddingYGeral)] py-[var(--paddingYGeralCel)]">
-            <a href="https://www.conexaoastralmistica/AdmChat" target="_blank" className="text-white">Login atendente</a>
+            <a href="https://www.conexaoastralmistica.com.br/AdmChat" target="_blank" className="text-white">Login atendente</a>
         </footer>
     )
 }
