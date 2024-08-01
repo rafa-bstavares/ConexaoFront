dc1a5bdf9cd6caa4894f825f4be61db953f5d767
rafaTavDev <rafabstavares@gmail.com>
2024-08-01 12:43:22 -0300

    pagamento dentro consulta como popup

--- src/Components/ModalRecarregar/ModalRecarregar.tsx
+++ src/Components/ModalRecarregar/ModalRecarregar.tsx
@@ -38,8 +38,8 @@ export default function ModalRecarregar({minutosRestantes}: Props){
     
 
     return(
-        <div className="fixed bg-white/90 h-screen w-full top-0 left-0 flex justify-center items-center">
-            <div className="flex flex-col gap-2 lg:gap-4 px-4 lg:px-8 py-3 lg:py-6 bg-roxoPrincipal w-[90%] lg:w-1/2 rounded-md text-white">
+        <div className="fixed bg-white/90 h-[30%] w-[40%] top-2 left-2 flex justify-center items-center">
+            <div className="flex flex-col gap-2 lg:gap-4 px-4 lg:px-8 py-3 lg:py-6 bg-roxoPrincipal w-[90%] max-h-[90%] overflow-y-scroll rounded-md text-white">
                 <div>Faltam {minutosRestantes} minutos para acabar sua consulta, deseja prolongar o tempo da consulta?</div>
                 <div>*Será calculado o valor para o tempo adicional e ele será retirado do seu saldo.</div>
                 <div className="flex flex-col lg:flex-row gap-2 lg:gap-4 self-center mt-3 lg:mt-5">
--- src/Components/PagamentoDentroConsulta/PagamentoDentroConsulta.tsx
+++ src/Components/PagamentoDentroConsulta/PagamentoDentroConsulta.tsx
@@ -190,8 +190,8 @@ export default function PagamentoDentroConsulta(){
     
     return (
         <div>
-            <div className="fixed bg-white/90 h-screen w-full top-0 left-0 flex justify-center items-center">
-                <div className="flex flex-col gap-2 lg:gap-4 px-4 lg:px-8 py-3 lg:py-6 bg-roxoPrincipal w-[90%] max-h-[90vh] lg:w-1/2 rounded-md text-white overflow-y-scroll">
+            <div className="fixed bg-white/90 h-[30%] w-[40%] top-2 left-2 flex justify-center items-center">
+                <div className="flex flex-col gap-2 lg:gap-4 px-4 lg:px-8 py-3 lg:py-6 bg-roxoPrincipal w-[90%] max-h-[90%] rounded-md text-white overflow-y-scroll">
                     <div className="w-2/3 lg:w-1/4 self-center">
                         <img src={imgLogo} alt="logo" className="w-full h-auto" />
                     </div>
