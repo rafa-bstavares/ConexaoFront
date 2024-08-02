2980792f19199b8e3e6c4a37a71c080b1e96f960
rafaTavDev <rafabstavares@gmail.com>
2024-08-02 14:26:29 -0300

    ajeitando modais pagamentop 02

--- src/Components/ModalRecarregar/ModalRecarregar.tsx
+++ src/Components/ModalRecarregar/ModalRecarregar.tsx
@@ -38,7 +38,7 @@ export default function ModalRecarregar({minutosRestantes}: Props){
     
 
     return(
-        <div className="fixed bg-white/90 h-[30%] w-[40%] top-2 left-2 flex justify-center items-center">
+        <div className="fixed bg-white/90 lg:h-[30%] lg:w-[40%] w-full h-auto top-2 left-2 flex justify-center items-center">
             <div className="flex flex-col gap-2 lg:gap-4 px-4 lg:px-8 py-3 lg:py-6 bg-roxoPrincipal w-[90%] max-h-[90%] overflow-y-scroll rounded-md text-white">
                 <div>Faltam {minutosRestantes} minutos para acabar sua consulta, deseja prolongar o tempo da consulta?</div>
                 <div>*Será calculado o valor para o tempo adicional e ele será retirado do seu saldo.</div>
