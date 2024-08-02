020a6c175c0fa91a345ba6ed34580f9c333c579e
rafaTavDev <rafabstavares@gmail.com>
2024-08-02 11:40:44 -0300

    modal historico atendente 03

--- src/Components/ModalHistorico/ModalHistorico.tsx
+++ src/Components/ModalHistorico/ModalHistorico.tsx
@@ -27,7 +27,7 @@ export default function ModalHistorico(){
 
     return (
         <div className="bg-roxoPrincipal rounded-xl flex flex-col px-4 lg:px-12 lg:py-8 py-2 gap-4 justify-center items-center overflow-y-scroll">
-            <div className="flex gap-2 py-4 self-start" onClick={() => setAbrirModalHistorico(false)}>
+            <div className="flex gap-2 py-4 self-start items-center cursor-pointer" onClick={() => setAbrirModalHistorico(false)}>
                 <img src={setaVoltar} alt="voltar" className="h-auto w-[10vw] lg:w-[2vw] rotate-90" />
                 <div>voltar</div>
             </div>
