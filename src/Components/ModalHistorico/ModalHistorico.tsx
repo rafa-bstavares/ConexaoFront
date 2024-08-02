a3ee5bb1beb410f3a6d5d83ef1ece8f46ef588f8
rafaTavDev <rafabstavares@gmail.com>
2024-08-02 11:35:13 -0300

    voltar modal historico atendente

--- src/Components/ModalHistorico/ModalHistorico.tsx
+++ src/Components/ModalHistorico/ModalHistorico.tsx
@@ -3,6 +3,7 @@ import { ContextoProfissionais } from "../../Contexts/ContextoProfissionais/Cont
 import { ContextoAviso } from "../../Contexts/ContextoAviso/ContextoAviso"
 import ItemModalHistorico from "../ItemModalHistorico/ItemModalHistorico"
 import Botao from "../Botao/Botao"
+import setaVoltar from "../../assets/images/setaSeletor.svg"
 
 
 export default function ModalHistorico(){
@@ -26,6 +27,10 @@ export default function ModalHistorico(){
 
     return (
         <div className="bg-roxoPrincipal rounded-xl flex flex-col px-4 lg:px-12 lg:py-8 py-2 gap-4 justify-center items-center overflow-y-scroll">
+            <div className="flex gap-2 py-4" onClick={() => setAbrirModalHistorico(false)}>
+                <img src={setaVoltar} alt="voltar" className="h-auto w-1/6 lg:w-[5vw]" />
+                <div>voltar</div>
+            </div>
             {
                 arrHistoricosAtendente.map(item => (
                     <ItemModalHistorico historico={item.historico} nomeCliente={item.nomeCliente} data={item.data} nomeAtendente={perfilProAtual.nome}/>
